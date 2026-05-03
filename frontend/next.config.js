0ec1c096e28eb715d20c7ec6b71c09823f27620d
martascorreia <correiamarta2@gmail.com>
2026-05-03 22:03:50 +0100

    fix: add github pages deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true
+  }
+};
+
+export default nextConfig;
